9296f85d9aaaba39d3a08ae8cd28383c6a6e3a4e
yujiseok <koesijuy@gmail.com>
2023-06-25 15:41:30 +0900

    feat: `exchange` 타입 추가

---
+++ src/types/exchanges.d.ts
@@ -0,0 +1,52 @@
+interface IExchange {
+  id: string;
+  name: string;
+  year_established: number;
+  country: string;
+  description: string;
+  url: string;
+  image: string;
+  has_trading_incentive: boolean;
+  trust_score: number;
+  trust_score_rank: number;
+  trade_volume_24h_btc: number;
+  trade_volume_24h_btc_normalized: number;
+}
+
+interface ITicker {
+  name: string;
+  tickers: Ticker[];
+}
+
+interface Ticker {
+  base: string;
+  target: string;
+  market: {
+    name: string;
+    identifier: string;
+    has_trading_incentive: boolean;
+  };
+  last: number;
+  volume: number;
+  converted_last: {
+    btc: number;
+    eth: number;
+    usd: number;
+  };
+  converted_volume: {
+    btc: number;
+    eth: number;
+    usd: number;
+  };
+  trust_score: string;
+  bid_ask_spread_percentage: number;
+  timestamp: string;
+  last_traded_at: string;
+  last_fetch_at: string;
+  is_anomaly: boolean;
+  is_stale: boolean;
+  trade_url: string;
+  token_info_url: any;
+  coin_id: string;
+  target_coin_id?: string;
+}
