183cb99249a6a184d2199d12e701b81820cf7c39
yujiseok <koesijuy@gmail.com>
2023-06-25 16:40:35 +0900

    feat: active 스타일 추가

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -18,18 +18,32 @@ const Pagination = ({ totalPage, pageArr, page, setPage }: PaginationProps) => {
   };
 
   return (
-    <div className="my-8 flex gap-2">
-      <button disabled={page === 1} onClick={handleClickNextBtn}>
+    <div className="my-8 flex justify-center gap-2">
+      <button
+        disabled={page === 1}
+        onClick={handleClickNextBtn}
+        className="disabled:pointer-events-none disabled:opacity-50"
+      >
         prev
       </button>
 
       {pageArr.map((_, i) => (
-        <button key={i} onClick={() => setPage(i + 1)}>
+        <button
+          key={i}
+          onClick={() => setPage(i + 1)}
+          className={`${
+            page === i + 1 ? "bg-code-horizon-blue text-white" : ""
+          } rounded-md px-3 py-2 `}
+        >
           {i + 1}
         </button>
       ))}
 
-      <button disabled={page === totalPage} onClick={handleClickPrevBtn}>
+      <button
+        disabled={page === totalPage}
+        onClick={handleClickPrevBtn}
+        className="disabled:pointer-events-none disabled:opacity-50"
+      >
         next
       </button>
     </div>
