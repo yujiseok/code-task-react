1fa6e821954c8af3d47519cea980478941a7f4f0
yujiseok <koesijuy@gmail.com>
2023-06-25 20:57:22 +0900

    refactor: `getExchange` 엔드포인트 및 반환값 수정
    - ticker -> exchanges
    - totalPage 반환

--- src/api/exchanges.ts
+++ src/api/exchanges.ts
@@ -1,3 +1,4 @@
+import { IExchange, ITicker } from "../types/exchanges";
 import instance from "./instance";
 
 export const getAllExchanges = async (page: number) => {
@@ -11,8 +12,11 @@ export const getAllExchanges = async (page: number) => {
 };
 
 export const getExchange = async (id: string) => {
-  const res = await instance.get(`/exchanges/${id}/tickers`);
+  const res = await instance.get(`/exchanges/${id}`);
   const data: ITicker = res.data;
+  const perPage = res.headers["per-page"];
+  const total = res.headers.total;
+  const totalPage = Math.ceil(total / perPage);
 
-  return data;
+  return { data, totalPage };
 };
