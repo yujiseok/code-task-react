0394febf7dfa26c46ed37113cfc656bc6d3205b7
yujiseok <koesijuy@gmail.com>
2023-06-25 16:41:36 +0900

    feat: `code` 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "code-horizon-blue": "#56B9C0",
+        "code-black": "#2F2F32",
+        "code-dark-blue": "#0B425E",
+      },
+    },
   },
   plugins: [],
 };
