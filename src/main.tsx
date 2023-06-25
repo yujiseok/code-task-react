47abaaabc2cdbde0c1b116c738ded5b652445d6f
yujiseok <koesijuy@gmail.com>
2023-06-25 16:15:32 +0900

    feat: `Suspense` 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App.tsx";
 import "./index.css";
+import Skeleton from "./components/Skeleton.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <main className="grid min-h-screen place-items-center px-4 py-6">
+        <Suspense fallback={<Skeleton />}>
+          <App />
+        </Suspense>
+      </main>
     </RecoilRoot>
   </React.StrictMode>
 );
