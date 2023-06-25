c63977e1893df6b028fb66d43d6d0a1fdfb5ed85
yujiseok <koesijuy@gmail.com>
2023-06-25 23:20:54 +0900

    feat:  폰트 적용
    - DM Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif", "Roboto", "Arial"],
+      },
       colors: {
         "code-horizon-blue": "#56B9C0",
         "code-black": "#2F2F32",
